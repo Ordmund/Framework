74fd4538870b84edab29f631cafe36403914c8fa
Ordmund <vladyslav.alvgeir@gmail.com>
2026-01-29 14:12:47 +0900

    Added VectorExtensions.

---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Core.Extensions
+{
+	public static class VectorExtensions
+	{
+		public static Vector3 ClampToBounds(this Vector3 vector, Bounds bounds)
+		{
+			return new Vector3(Mathf.Clamp(vector.x, bounds.min.x, bounds.max.x), 
+				Mathf.Clamp(vector.y, bounds.min.y, bounds.max.y), 
+				Mathf.Clamp(vector.z, bounds.min.z, bounds.max.z));
+		}
+	}
+}
