cd587d18790229259866b422c02544dd16a69406
Ordmund <vladyslav.alvgeir@gmail.com>
2026-02-05 18:49:16 +0900

    Added base DefinitionLibrary logic.

---
+++ Abstract/DefinitionLibrary.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using System.Linq;
+#endif
+
+namespace Core.Abstract
+{
+	public abstract class DefinitionLibrary<T> : ScriptableObject where T : ScriptableObject
+	{
+		[SerializeField] protected List<T> _definitions;
+
+		protected abstract string PathToDefinitions { get; }
+
+		public List<T> Definitions => _definitions;
+
+		public virtual void ScanForDefinitions()
+		{
+#if UNITY_EDITOR
+			var guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { PathToDefinitions });
+			var items = guids.Select(guid => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid))).ToList();
+
+			_definitions = new List<T>();
+			foreach (var item in items)
+			{
+				_definitions.Add(item);
+			}
+
+			Debug.Log($"<color=green>{typeof(T).Name} Library updated with {_definitions.Count} definitions!</color>");
+#endif
+		}
+	}
+}
