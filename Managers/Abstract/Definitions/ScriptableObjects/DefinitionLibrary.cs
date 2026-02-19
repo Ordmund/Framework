7bc4327583e58841c857cdbde1925e167b6d016b
Ordmund <vladyslav.alvgeir@gmail.com>
2026-02-19 20:25:55 +0900

    Minor refactoring.

--- Managers/Abstract/Definitions/ScriptableObjects/DefinitionLibrary.cs
+++ Managers/Abstract/Definitions/ScriptableObjects/DefinitionLibrary.cs
@@ -9,11 +9,11 @@ namespace Core.Managers.Definitions
 {
 	public abstract class DefinitionLibrary<T> : ScriptableObject where T : ScriptableObject
 	{
-		[SerializeField] protected List<T> _definitions;
+		[SerializeField] protected List<T> definitions;
 
 		protected abstract string PathToDefinitions { get; }
 
-		public List<T> Definitions => _definitions;
+		public List<T> Definitions => definitions;
 
 		public virtual void ScanForDefinitions()
 		{
@@ -21,13 +21,13 @@ namespace Core.Managers.Definitions
 			var guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { PathToDefinitions });
 			var items = guids.Select(guid => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid))).ToList();
 
-			_definitions = new List<T>();
+			definitions = new List<T>();
 			foreach (var item in items)
 			{
-				_definitions.Add(item);
+				definitions.Add(item);
 			}
 
-			Debug.Log($"<color=green>{typeof(T).Name} Library updated with {_definitions.Count} definitions!</color>");
+			Debug.Log($"<color=green>{typeof(T).Name} Library updated with {definitions.Count} definitions!</color>");
 #endif
 		}
 	}
