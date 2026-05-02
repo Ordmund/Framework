52fa0f160b82e09e55ccb1d98f198a8eb350d0d0
Ordmund <vladyslav.alvgeir@gmail.com>
2026-05-02 14:53:13 +0900

    Added gameObject and transform caching in ViewBase.

--- MVC/Components/Abstract/ViewBase.cs
+++ MVC/Components/Abstract/ViewBase.cs
@@ -4,5 +4,15 @@ namespace Core.MVC
 {
 	public abstract class ViewBase : MonoBehaviour
 	{
+		private GameObject _gameObject;
+		private Transform _transform;
+
+		protected GameObject GameObject => _gameObject ??= gameObject;
+		protected Transform Transform => _transform ??= transform;
+
+		public void SetActive(bool isActive)
+		{
+			GameObject.SetActive(isActive);
+		}
 	}
 }
