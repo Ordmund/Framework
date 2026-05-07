799c9f2a261bb189b759205abcd5d9bf69520853
Ordmund <vladyslav.alvgeir@gmail.com>
2026-05-07 14:03:00 +0900

    Added MaskedBar for fillable bars.

---
+++ UI/MaskedBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.UI
+{
+	public class MaskedBar : MonoBehaviour
+	{
+		[SerializeField] private RectTransform _rectTransform;
+		[SerializeField] private RectMask2D _rectMask2D;
+
+		public void SetValue(float value)
+		{
+			var clampedValue = Mathf.Clamp01(value);
+
+			var padding = _rectMask2D.padding;
+			padding.z = _rectTransform.rect.width * (1 - clampedValue);
+
+			_rectMask2D.padding = padding;
+		}
+	}
+}
