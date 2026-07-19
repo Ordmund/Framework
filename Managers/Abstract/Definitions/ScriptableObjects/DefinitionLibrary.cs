e7409022d3c3f4de8344407345b453257e9cdf7d
Ordmund <vladyslav.alvgeir@gmail.com>
2026-07-19 15:08:51 +0900

    Replaced raw string path usage with linked folder for stability purposes.

--- Managers/Abstract/Definitions/ScriptableObjects/DefinitionLibrary.cs
+++ Managers/Abstract/Definitions/ScriptableObjects/DefinitionLibrary.cs
@@ -9,16 +9,20 @@ namespace Framework.Managers.Definitions
 {
 	public abstract class DefinitionLibrary<T> : ScriptableObject where T : ScriptableObject
 	{
+#if UNITY_EDITOR
+		[SerializeField] private DefaultAsset _definitionsFolder;
+#endif
 		[SerializeField] protected List<T> definitions;
-
-		protected abstract string PathToDefinitions { get; }
 
 		public IReadOnlyList<T> Definitions => definitions;
 
 		public virtual void ScanForDefinitions()
 		{
 #if UNITY_EDITOR
-			var guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { PathToDefinitions });
+			if (!TryGetDefinitionsFolderPath(out var path))
+				return;
+
+			var guids = AssetDatabase.FindAssets($"t:{typeof(T).Name}", new[] { path });
 			var items = guids.Select(guid => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid))).ToList();
 
 			definitions = new List<T>();
@@ -31,6 +35,29 @@ namespace Framework.Managers.Definitions
 			AssetDatabase.SaveAssets();
 
 			Debug.Log($"<color=green>{typeof(T).Name} Library updated with {definitions.Count} definitions!</color>");
+#endif
+		}
+
+		protected bool TryGetDefinitionsFolderPath(out string path)
+		{
+#if UNITY_EDITOR
+			path = null;
+
+			if (_definitionsFolder == null)
+			{
+				Debug.LogError($"<color=red>Path to {typeof(T).Name} definitions folder is not set!</color>");
+				return false;
+			}
+
+			var assetPath = AssetDatabase.GetAssetPath(_definitionsFolder);
+			if (!AssetDatabase.IsValidFolder(assetPath))
+			{
+				Debug.LogError($"<color=red>Path to {typeof(T).Name} definitions folder is not valid!</color>");
+				return false;
+			}
+
+			path = assetPath;
+			return true;
 #endif
 		}
 	}
