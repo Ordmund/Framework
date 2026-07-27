9dda5bcdcd7cfec6657643e70b8d3b85485b1762
Ordmund <vladyslav.alvgeir@gmail.com>
2026-07-27 22:34:34 +0900

    Minor naming change.

--- Managers/Static/ResourcesManager.cs
+++ Managers/Static/ResourcesManager.cs
@@ -6,7 +6,7 @@ namespace Framework.Managers
 {
 	public static class ResourcesManager
 	{
-		private static readonly Dictionary<string, Object> _assets = new();
+		private static readonly Dictionary<string, Object> Assets = new();
 
 		/// <summary>
 		/// Loads asset with specified type by specified path
@@ -16,11 +16,11 @@ namespace Framework.Managers
 		/// <returns>Loaded asset</returns>
 		public static T Load<T>(string path) where T : Object
 		{
-			if (_assets.TryGetValue(path, out var preloadedAsset))
+			if (Assets.TryGetValue(path, out var preloadedAsset))
 				return preloadedAsset as T;
 
 			var asset = Resources.Load<T>(path);
-			_assets.Add(path, asset);
+			Assets.Add(path, asset);
 
 			return asset;
 		}
@@ -31,9 +31,9 @@ namespace Framework.Managers
 		/// <param name="asset">Asset that need to be unloaded</param>
 		public static void Unload(Object asset)
 		{
-			var assetPair = _assets.FirstOrDefault(pair => pair.Value == asset);
+			var assetPair = Assets.FirstOrDefault(pair => pair.Value == asset);
 			if (!string.IsNullOrEmpty(assetPair.Key))
-				_assets.Remove(assetPair.Key);
+				Assets.Remove(assetPair.Key);
 
 			Resources.UnloadAsset(asset);
 		}
@@ -44,13 +44,13 @@ namespace Framework.Managers
 		/// <param name="path">Path to asset that need to be unloaded</param>
 		public static void Unload(string path)
 		{
-			if (_assets.ContainsKey(path))
+			if (Assets.ContainsKey(path))
 			{
-				var asset = _assets[path];
+				var asset = Assets[path];
 				if (asset is not GameObject)
 					Resources.UnloadAsset(asset);
 
-				_assets.Remove(path);
+				Assets.Remove(path);
 			}
 		}
 
@@ -59,11 +59,11 @@ namespace Framework.Managers
 		/// </summary>
 		public static void UnloadAll()
 		{
-			var assetsToUnload = _assets.Where(asset => asset.Value is not GameObject);
+			var assetsToUnload = Assets.Where(asset => asset.Value is not GameObject);
 			foreach (var asset in assetsToUnload)
 				Resources.UnloadAsset(asset.Value);
 
-			_assets.Clear();
+			Assets.Clear();
 
 			Resources.UnloadUnusedAssets();
 		}
